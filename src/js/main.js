e42f1635beb2d19885b6ba9c9246050264beef4e
Marcus Johansson <arcus.johansson@gmail.com>
2015-10-01 13:35:04 +0200

    Estrad - Loreen

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	//Slide toggle mobile menu
+	$('.menu button').click(function() {
+		$('.main-menu > ul').slideToggle("slow");
+	});
+
+	$(".fancybox").fancybox();
+	$(".fancybox-1").fancybox();
+});
+
+
+
